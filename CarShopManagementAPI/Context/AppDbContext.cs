02ac625f214ddce0dd66fa66afaec52ec3e83310
Carlos Abraão <carlosabraao@hotmail.com.br>
2024-08-13 11:17:38 -0300

    adding connection to MySQL database

---
+++ CarShopManagementAPI/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarShopManagementAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+            
+        }
+    }
+}
