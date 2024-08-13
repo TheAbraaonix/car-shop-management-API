08f2f0ebe58ee3007ae98558046300644f8af3d5
Carlos Abraão <carlosabraao@hotmail.com.br>
2024-08-13 19:00:28 -0300

    implementing remaining controller methods

--- CarShopManagementAPI/Controllers/CarController.cs
+++ CarShopManagementAPI/Controllers/CarController.cs
@@ -1,4 +1,5 @@
-﻿using CarShopManagementAPI.Repositories;
+﻿using CarShopManagementAPI.Models;
+using CarShopManagementAPI.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarShopManagementAPI.Controllers
@@ -19,6 +20,41 @@ namespace CarShopManagementAPI.Controllers
         public async Task<IActionResult> GetAllCars()
         {
             return Ok(await _repository.GetAllCarsAsync());
+        }
+
+        [HttpGet]
+        [Route("GetCarById/{id}")]
+        public async Task<IActionResult> GetCarById(Guid id)
+        {
+            Car car = await _repository.GetCarByIdAsync(id);
+
+            if (car == null) return NotFound();
+
+            return Ok(car);
+        }
+
+        [HttpPost]
+        [Route("CreateCar")]
+        public async Task<IActionResult> CreateCar([FromBody] Car car)
+        {
+            Car createdCar = await _repository.CreateCarAsync(car);
+            return CreatedAtAction(nameof(CreateCar), new Car { Id = createdCar.Id }, createdCar);
+        }
+
+        [HttpPut]
+        [Route("UpdateCar")]
+        public async Task<IActionResult> UpdateCar(Car car)
+        {
+            Car updatedCar = await _repository.UpdateCarAsync(car);
+            return Ok(updatedCar);
+        }
+
+        [HttpDelete]
+        [Route("DeleteCar")]
+        public async Task<IActionResult> DeleteCar(Car car)
+        {
+            await _repository.DeleteCarAsync(car);
+            return NoContent();
         }
     }
 }
